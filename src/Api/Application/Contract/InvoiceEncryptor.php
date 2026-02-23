4e304c9afd045736196e719e8bfe23758bb4a7eb
JakubSzczerba <jakszczerb@gmail.com>
2026-02-23 20:58:35 +0100

    port for encrypting session and invoice

---
+++ src/Api/Application/Contract/InvoiceEncryptor.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+ */
+
+declare(strict_types=1);
+
+namespace Ksef\Api\Application\Contract;
+
+use Ksef\Api\Domain\Invoice\EncryptedInvoice;
+use Ksef\Api\Domain\Invoice\SessionEncryptionData;
+
+interface InvoiceEncryptor
+{
+    public function createSessionEncryptionData(): SessionEncryptionData;
+
+    public function encryptInvoice(string $invoiceXml, SessionEncryptionData $sessionEncryptionData): EncryptedInvoice;
+}
+
